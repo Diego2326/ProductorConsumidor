0f5dfc7c0fffb2b7e1b821ba4038efd6a3766738
Diego2326 <127332447+Diego2326@users.noreply.github.com>
2025-08-23 08:02:46 -0600

    Initial Commit

---
+++ ProductorConsumidor macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  ProductorConsumidor macOS
+//
+//  Created by Diego on 23/8/25.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
